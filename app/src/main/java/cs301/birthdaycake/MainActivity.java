8377ece2e9d09c968879c8a3904b7c35d8f86a7f
Aurora <kathryn.aurora.w@gmail.com>
2022-03-07 16:38:07 -0800

    Switches are on, seekbar caps at 5, seekbar prgrss is 2, prints goodbye message when goodbye button is clicked

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -3,6 +3,8 @@ package cs301.birthdaycake;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goodbye(View button){
+        Log.i("button","Goodbye");
     }
 }

5e1cc46872e4c06af88722b69a988c5fe590bc5c
Aurora <kathryn.aurora.w@gmail.com>
2022-09-13 17:22:41 -0700

    Default Changelist?

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goodbye(View button) {
-        System.out.println("Byebye!");
         Log.i("button","Goodbye!");
     }
 }

87894f31e8ed5e04af138f1a8e4293af4d12dda5
Aurora <kathryn.aurora.w@gmail.com>
2022-09-29 16:32:42 -0700

    Balloon Class

---
+++ app/src/main/java/cs301/birthdaycake/Balloon.java
@@ -0,0 +1,17 @@
+package cs301.birthdaycake;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Balloon {
+
+
+    //private float x, y;
+    private float bX, bY;
+
+    public Balloon(float x, float y) {
+        bX = x;
+        bY = y;
+    }
+
+}
